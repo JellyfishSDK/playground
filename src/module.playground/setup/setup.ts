16f4ba040f6b72bc442efd806d659588743de25a
Fuxing Loh <git@fuxing.dev>
2021-05-26 20:24:04 +0800

    added code docs

--- src/module.playground/setup/setup.ts
+++ src/module.playground/setup/setup.ts
@@ -14,6 +14,9 @@ export abstract class PlaygroundSetup<Each> {
     }
   }
 
+  /**
+   * @return {string} address that should be used for everything
+   */
   static get address (): string {
     return PlaygroundSetup.MN_KEY.operator.address
   }
@@ -21,6 +24,9 @@ export abstract class PlaygroundSetup<Each> {
   constructor (protected readonly client: JsonRpcClient) {
   }
 
+  /**
+   * Run the setup process
+   */
   async setup (): Promise<void> {
     const list = this.list()
     await this.before(list)
@@ -36,18 +42,36 @@ export abstract class PlaygroundSetup<Each> {
     await this.after(list)
   }
 
+  /**
+   * Before creating each, optionally execute something
+   */
   protected async before (list: Each[]): Promise<void> {
   }
 
+  /**
+   * After creating each, optionally execute something
+   */
   protected async after (list: Each[]): Promise<void> {
   }
 
+  /**
+   * @return {Each[]} to setup
+   */
   abstract list (): Each[]
 
+  /**
+   * @param {Each} each to check if it already exist so that it won't be setup again.
+   */
   abstract has (each: Each): Promise<boolean>
 
+  /**
+   * @param {Each} each to create
+   */
   abstract create (each: Each): Promise<void>
 
+  /**
+   * @param {number} count of blocks to generate
+   */
   protected async generate (count: number): Promise<void> {
     let current = await this.client.blockchain.getBlockCount()
     const target = current + count
@@ -57,6 +81,9 @@ export abstract class PlaygroundSetup<Each> {
     }
   }
 
+  /**
+   * @param {number} balance to wait for wallet to reach
+   */
   protected async waitForBalance (balance: number): Promise<void> {
     let current = await this.client.wallet.getBalance()
     while (current.lt(balance)) {
