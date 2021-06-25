5650d7f7d5b0ede6309b8aefcdf7c2ff99f41355
Ahmed Hilali <monstrobishi@gmail.com>
2021-06-25 16:19:25 +0800

    Use bech32 MN keys and owner address (#52)
    Co-authored-by: Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>

--- src/module.playground/setup/setup.oracle.ts
+++ src/module.playground/setup/setup.oracle.ts
@@ -10,10 +10,12 @@ interface OracleSetup {
 
 @Injectable()
 export class SetupOracle extends PlaygroundSetup<OracleSetup> {
+  oracleOwnerAddress: string = 'bcrt1qurwyhta75n2g75u2u5nds9p6w9v62y8wr40d2r'
+
   list (): OracleSetup[] {
     return [
       {
-        address: PlaygroundSetup.address,
+        address: this.oracleOwnerAddress,
         priceFeeds: [
           {
             token: 'TSLA',
@@ -33,7 +35,7 @@ export class SetupOracle extends PlaygroundSetup<OracleSetup> {
         }
       },
       {
-        address: PlaygroundSetup.address,
+        address: this.oracleOwnerAddress,
         priceFeeds: [
           {
             token: 'TSLA',
@@ -53,7 +55,7 @@ export class SetupOracle extends PlaygroundSetup<OracleSetup> {
         }
       },
       {
-        address: PlaygroundSetup.address,
+        address: this.oracleOwnerAddress,
         priceFeeds: [
           {
             token: 'TSLA',
