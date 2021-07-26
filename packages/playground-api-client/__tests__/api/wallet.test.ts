b36b5fc091ff7c05b113bc95e066f73941a652c2
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-07-26 14:43:03 +0800

    added "wallet/tokens/:id/sendtoaddress" (#102)

--- packages/playground-api-client/__tests__/api/wallet.test.ts
+++ packages/playground-api-client/__tests__/api/wallet.test.ts
@@ -2,7 +2,6 @@ import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { PlaygroundApiClient } from '../../src'
 import { StubPlaygroundApiClient } from '../stub.client'
 import { StubService } from '../stub.service'
-import waitForExpect from 'wait-for-expect'
 
 let container: MasterNodeRegTestContainer
 let service: StubService
@@ -45,7 +44,7 @@ it('should get wallet', async () => {
   })
 })
 
-it('should dfi token send to address and wait for automated block confirmation', async () => {
+it('should dfi token send to address and wait for automated block confirmation (deprecated in favor of "wallet/tokens/0/sendtoaddress")', async () => {
   const address = 'bcrt1qgpu5k3v66qjf8lc4p4lny0uwdxv6vf94axnjkf'
 
   const txId = await client.wallet.sendTokenDfiToAddress({
@@ -55,8 +54,20 @@ it('should dfi token send to address and wait for automated block confirmation',
 
   expect(txId.length).toStrictEqual(64)
 
-  await waitForExpect(async () => {
-    const balances = await container.call('getaccount', [address])
-    expect(balances).toStrictEqual(['10.00000000@DFI'])
+  const balances = await container.call('getaccount', [address])
+  expect(balances).toStrictEqual(['10.00000000@DFI'])
+})
+
+it('should send token 0 to address and wait for automated block confirmation', async () => {
+  const address = 'bcrt1qkt7rvkzk8qs7rk54vghrtzcdxfqazscmmp30hk'
+
+  const txId: string = await client.requestData('POST', 'wallet/tokens/0/sendtoaddress', {
+    address: address,
+    amount: '15.99134567'
   })
+
+  expect(txId.length).toStrictEqual(64)
+
+  const balances = await container.call('getaccount', [address])
+  expect(balances).toStrictEqual(['15.99134567@DFI'])
 })
--- src/module.api/wallet.controller.ts
+++ src/module.api/wallet.controller.ts
@@ -7,11 +7,13 @@ import {
   CTransaction,
   DeFiTransactionConstants,
   OP_CODES,
+  Script,
   Transaction,
   Vin,
   Vout
 } from '@defichain/jellyfish-transaction'
 import { DeFiAddress } from '@defichain/jellyfish-address'
+import { UTXO } from '@defichain/jellyfish-api-core/dist/category/wallet'
 
 @Controller('/v0/playground/wallet')
 export class WalletController {
@@ -38,19 +40,44 @@ export class WalletController {
     }
   }
 
+  /**
+   * @deprecated
+   */
   @Post('/tokens/dfi/sendtoaddress')
+  async postDFIFallback (@Body() data: TokenDfiToAddress): Promise<string> {
+    return await this.post(data)
+  }
+
+  @Post('/tokens/0/sendtoaddress')
   async post (@Body() data: TokenDfiToAddress): Promise<string> {
     const amount = new BigNumber(data.amount)
     const script = DeFiAddress.from('regtest', data.address).getScript()
+    const utxos = await this.queryUnspent(amount)
 
-    const utxos = await this.client.wallet.listUnspent(1, 9999999, {
+    const transaction = this.createUtxoToAccountTransaction(amount, script, utxos)
+    const hex = new CTransaction(transaction).toHex()
+    const signed = await this.client.rawtx.signRawTransactionWithKey(hex, [PlaygroundSetup.privKey])
+    const txId = await this.client.rawtx.sendRawTransaction(signed.hex, new BigNumber('100'))
+
+    await this.waitConfirmation(txId)
+    return txId
+  }
+
+  /**
+   * Get unspent to spend and locks it.
+   */
+  async queryUnspent (amount: BigNumber): Promise<UTXO[]> {
+    return await this.client.wallet.listUnspent(1, 9999999, {
       addresses: [PlaygroundSetup.address],
+      includeUnsafe: false,
       queryOptions: {
         maximumCount: 100,
         minimumSumAmount: amount.plus(1).toNumber()
       }
     })
+  }
 
+  createUtxoToAccountTransaction (amount: BigNumber, script: Script, utxos: UTXO[]): Transaction {
     const utxoToAccount: Vout = {
       value: amount,
       script: {
@@ -74,7 +101,7 @@ export class WalletController {
       tokenId: 0x00
     }
 
-    const transaction: Transaction = {
+    return {
       version: DeFiTransactionConstants.Version,
       vin: utxos.map((utxo): Vin => ({
         index: utxo.vout,
@@ -88,9 +115,26 @@ export class WalletController {
       ],
       lockTime: 0x00000000
     }
+  }
 
-    const hex = new CTransaction(transaction).toHex()
-    const signed = await this.client.rawtx.signRawTransactionWithKey(hex, [PlaygroundSetup.privKey])
-    return await this.client.rawtx.sendRawTransaction(signed.hex, new BigNumber('100'))
+  async waitConfirmation (txId: string): Promise<void> {
+    /* eslint-disable no-void */
+    const expiredAt = Date.now() + 10000 // 10 seconds
+
+    return await new Promise((resolve, reject) => {
+      const checkCondition = async (): Promise<void> => {
+        const { confirmations } = await this.client.rawtx.getRawTransaction(txId, true)
+        if (confirmations > 0) {
+          resolve()
+        } else if (expiredAt < Date.now()) {
+          reject(new Error('waitConfirmation timeout after 10 seconds'))
+        } else {
+          setTimeout(() => void checkCondition(), 500)
+        }
+      }
+
+      void checkCondition()
+    })
+    /* eslint-enable no-void */
   }
 }
