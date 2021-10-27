3cd18485721f72b64001ecf1c190ffb681e56e1b
Fuxing Loh <git@fuxing.dev>
2021-10-28 02:45:17 +0800

    fixed test

--- src/module.playground/bot/oracle.bot.e2e.ts
+++ src/module.playground/bot/oracle.bot.e2e.ts
@@ -25,41 +25,38 @@ describe('oracle bot', () => {
     const oracles = await client.oracle.listOracles()
     expect(Object.values(oracles).length).toBe(6)
 
-    // Wait for block 157
-    await waitForExpect(async () => {
-      const blocks = await client.blockchain.getBlockCount()
-      expect(blocks).toBeGreaterThan(157)
-    }, 200000)
-
     const setupOracle = app.get(SetupOracle)
     const oracleIds = setupOracle.oracleIds.U10
-    const oracleData = await client.oracle.getOracleData(oracleIds[0])
 
-    const tokenPriceU10 = oracleData.tokenPrices.find(x => x.token === 'U10')
-    expect(tokenPriceU10?.amount).toStrictEqual(11)
+    await waitForExpect(async () => {
+      const oracleData = await client.oracle.getOracleData(oracleIds[0])
 
-    const tokenPriceU25 = oracleData.tokenPrices.find(x => x.token === 'U25')
-    expect(tokenPriceU25?.amount).toStrictEqual(12.5)
+      const tokenPriceU10 = oracleData.tokenPrices.find(x => x.token === 'U10')
+      expect(tokenPriceU10?.amount).toStrictEqual(11)
 
-    const tokenPriceU50 = oracleData.tokenPrices.find(x => x.token === 'U50')
-    expect(tokenPriceU50?.amount).toStrictEqual(15)
+      const tokenPriceU25 = oracleData.tokenPrices.find(x => x.token === 'U25')
+      expect(tokenPriceU25?.amount).toStrictEqual(12.5)
 
-    const tokenPriceD10 = oracleData.tokenPrices.find(x => x.token === 'D10')
-    expect(tokenPriceD10?.amount).toStrictEqual(9000000)
+      const tokenPriceU50 = oracleData.tokenPrices.find(x => x.token === 'U50')
+      expect(tokenPriceU50?.amount).toStrictEqual(15)
 
-    const tokenPriceD25 = oracleData.tokenPrices.find(x => x.token === 'D25')
-    expect(tokenPriceD25?.amount).toStrictEqual(7500000)
+      const tokenPriceD10 = oracleData.tokenPrices.find(x => x.token === 'D10')
+      expect(tokenPriceD10?.amount).toStrictEqual(9000000)
 
-    const tokenPriceD50 = oracleData.tokenPrices.find(x => x.token === 'D50')
-    expect(tokenPriceD50?.amount).toStrictEqual(5000000)
+      const tokenPriceD25 = oracleData.tokenPrices.find(x => x.token === 'D25')
+      expect(tokenPriceD25?.amount).toStrictEqual(7500000)
 
-    const tokenPriceS25 = oracleData.tokenPrices.find(x => x.token === 'S25')
-    expect(tokenPriceS25?.amount).toStrictEqual(25)
+      const tokenPriceD50 = oracleData.tokenPrices.find(x => x.token === 'D50')
+      expect(tokenPriceD50?.amount).toStrictEqual(5000000)
 
-    const tokenPriceS50 = oracleData.tokenPrices.find(x => x.token === 'S50')
-    expect(tokenPriceS50?.amount).toStrictEqual(50)
+      const tokenPriceS25 = oracleData.tokenPrices.find(x => x.token === 'S25')
+      expect(tokenPriceS25?.amount).toStrictEqual(25)
 
-    const tokenPriceS100 = oracleData.tokenPrices.find(x => x.token === 'S100')
-    expect(tokenPriceS100?.amount).toStrictEqual(100)
+      const tokenPriceS50 = oracleData.tokenPrices.find(x => x.token === 'S50')
+      expect(tokenPriceS50?.amount).toStrictEqual(50)
+
+      const tokenPriceS100 = oracleData.tokenPrices.find(x => x.token === 'S100')
+      expect(tokenPriceS100?.amount).toStrictEqual(100)
+    }, 200000)
   })
 })
