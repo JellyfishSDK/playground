cde46d108b69915572c4feda88030286334f491e
Fuxing Loh <git@fuxing.dev>
2021-11-22 11:31:35 +0800

    fix broken tests

--- src/module.playground/bot/oracle.bot.ts
+++ src/module.playground/bot/oracle.bot.ts
@@ -135,7 +135,7 @@ export class OracleBot {
   constructor (readonly client: JsonRpcClient) {
   }
 
-  @Interval(5000)
+  @Interval(10000)
   async run (): Promise<void> {
     for (const oracleId of this.oracleIds) {
       await this.publish(oracleId)
